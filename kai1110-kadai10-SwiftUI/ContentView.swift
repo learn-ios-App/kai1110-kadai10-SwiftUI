a27fcd033d049cdac8f3927343f1f6fc2be17dab
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-07 17:48:44 +0900

    commit

--- kai1110-kadai10-SwiftUI/ContentView.swift
+++ kai1110-kadai10-SwiftUI/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  kai1110-kadai10-SwiftUI
-//
-//  Created by 渡邊魁優 on 2022/12/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀見", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県" ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(0 ..< prefectures.count, id: \.self) { index in
+                    HStack {
+                        Text(prefectures[index])
+                        Spacer()
+                        Text("\(index + 1)番目の都道府県です")
+                    }
+                    .listRowBackground(decisionColor(number: index))
+                }
+            }
+            .listStyle(InsetListStyle())
         }
-        .padding()
+    }
+    func decisionColor(number: Int) -> Color {
+        if(number % 3 == 0) {
+            return Color.red
+        }
+        else if(number % 3 == 1) {
+            return Color.green
+        }
+        else {
+            return Color.blue
+        }
     }
 }
 
